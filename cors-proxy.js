7052ba8a01b5dc649f5fb12ae875edadbfd107ff
albertusjuan <ajszy168@gmail.com>
2026-01-21 11:21:09 +0800

    fix: add x-tenant-subdomain to CORS allowed headers

--- cors-proxy.js
+++ cors-proxy.js
@@ -10,7 +10,7 @@ const TARGET = 'https://hub.wyniai.com';
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-tenant-subdomain');
     
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
